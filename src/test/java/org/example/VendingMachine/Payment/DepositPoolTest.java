f78cb409aa3e51f79e8bdbb222fe0c0dc8c50643
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2021-05-11 16:20:33 +0200

    Framework. Defining classes and interface. DepositPool done and test of depositPool done.

--- src/test/java/org/example/VendingMachine/Payment/DepositPoolTest.java
+++ src/test/java/org/example/VendingMachine/Payment/DepositPoolTest.java
@@ -1,4 +1,58 @@
+package org.example.VendingMachine.Payment;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DepositPoolTest {
-  
+
+    private DepositPool testDeposit = DepositPool.getInstance();
+
+    @BeforeEach
+    void setUp() {
+
+        testDeposit.clear();
+
+    }
+
+    @Test
+    void persist() {
+
+
+        int result = testDeposit.persist(5);
+        int expected = testDeposit.getDepositPool();
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void add() {
+        int result = testDeposit.persist(5 );
+        testDeposit.add(5);
+        int expected = 10;
+
+        assertEquals(expected,testDeposit.getDepositPool());
+    }
+
+    @Test
+    void getDepositPool() {
+
+        int expected = testDeposit.persist(15 );
+        int result = testDeposit.getDepositPool();
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void clear() {
+
+        testDeposit.persist(15 );
+        testDeposit.clear();
+        int result = testDeposit.getDepositPool();
+        int expected = 0;
+
+        assertEquals(expected,result);
+
+    }
 }
